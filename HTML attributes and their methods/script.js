90b0e1ffd5dcb5b0f983b38819929354f8b00371
Animesh Acharya <108991252+AnimeshA867@users.noreply.github.com>
2022-10-08 22:32:44 +0545

    Add files via upload

---
+++ HTML attributes and their methods/script.js
@@ -0,0 +1,33 @@
+let first= document.getElementById("first");
+
+// //This is the implementation of getAttribute method in javascript.
+// //getAttribute:
+
+// let a = first.getAttribute("class");
+
+// //This is the implementation of hasAttribute method in javascript.
+// //hasAttribute:
+
+// console.log(a);
+// console.log(first.hasAttribute("class"));
+// console.log(first.hasAttribute("style"));
+// console.log(first.hasAttribute("name"));
+// // document.getElementById("first").hidden=true;
+
+// //This is the implementation of setAttribute in javascript.
+// //Set attribute:
+// first.setAttribute("hidden","true")
+// first.setAttribute("class","true sahi");
+// console.log(a);
+
+
+// //removeAttribute:
+// //This is the implementation of removeAttribute in javaScript.
+
+// first.removeAttribute("hidden");
+
+// console.log(first.attributes);
+
+//dataset attibute:
+
+console.log(first.dataset);
